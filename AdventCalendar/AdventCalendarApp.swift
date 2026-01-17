ea2a32fcd552275789b482c0b9997abaf480dfd5
Meleshin Alexey <meleshinalexey5@gmail.com>
2026-01-18 00:19:04 +0300

    Initial Commit

---
+++ AdventCalendar/AdventCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventCalendarApp.swift
+//  AdventCalendar
+//
+//  Created by Alexey Meleshin on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct AdventCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
